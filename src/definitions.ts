22e812b7757339e64d977ff9b4c581bb3a03b487
maximilien0405 <maximilien.zimmermann@ik.me>
2025-09-04 19:13:31 +0200

    fix(app): Added comments on definitions.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,22 @@
 export interface AndroidLauncherPlugin {
+  /**
+   * Open the launcher settings page
+   */
   openLauncherSettings(): Promise<void>;
+
+  /**
+   * Start the immersive mode
+   * This method hides all system UI elements, creating a full-screen experience
+   */
   startImmersiveMode(): Promise<void>;
+
+  /**
+   * Stop the immersive mode
+   */
   stopImmersiveMode(): Promise<void>;
+
+  /**
+   * Check if the app is the default launcher or not
+   */
   isLauncherApp(): Promise<boolean>;
 }
